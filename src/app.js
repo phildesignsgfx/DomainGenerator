b7f0b507aa593d2cafee1c539ce4762584839f60
phildesignsgfx <phildesignsgfx@gmail.com>
2021-11-12 03:46:39 +0000

    domain done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function randomUrl() {
+  const pronoun = ["the", "our", "this"];
+  const adj = ["great", "big", "fantastic"];
+  const noun = ["jogger", "lion", "cat", "dog"];
+
+  function randomNames() {
+    const randompronoun = Math.floor(Math.random() * pronoun.length);
+    const randomadj = Math.floor(Math.random() * adj.length);
+    const randomnoun = Math.floor(Math.random() * noun.length);
+    const prValue = pronoun[randompronoun];
+    const aValue = adj[randomadj];
+    const nValue = noun[randomnoun];
+
+    const firstpart = document.querySelector(".firstpart");
+    firstpart.innerHTML = `<span = "firstpart">${prValue}${aValue}${nValue}</span>`;
+    // const secondpart = document.querySelector(".secondpart");
+    // secondpart.innerHTML = `<span = "second">${aValue} </span>`;
+    // const thirdpart = document.querySelector(".thirdpart");
+    // thirdpart.innerHTML = `<span = "thirdpart">${nValue} </span>`;
+  }
+
+  randomNames();
 };
